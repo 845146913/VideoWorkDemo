8d5b5d272d69256cc8252bcb561e9b48af3a8b7c
845146913 <845146913@qq.com>
2018-08-10 17:07:30 +0800

    换AForge实现

--- VideoWorkDemo/Form1.Designer.cs
+++ VideoWorkDemo/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
@@ -38,22 +42,68 @@
             this.panel1.Size = new System.Drawing.Size(318, 277);
             this.panel1.TabIndex = 0;
             // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(497, 27);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(275, 277);
+            this.videoSourcePlayer1.TabIndex = 1;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(558, 337);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(144, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(503, 340);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "设备：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(715, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "拍照";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(342, 334);
+            this.ClientSize = new System.Drawing.Size(837, 386);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.videoSourcePlayer1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- VideoWorkDemo/Form1.cs
+++ VideoWorkDemo/Form1.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
 
 namespace VideoWorkDemo
 {
@@ -17,10 +21,65 @@ namespace VideoWorkDemo
             InitializeComponent();
         }
 
+        //初始化
+        FilterInfoCollection videoDevices;
+        VideoCaptureDevice videoSource;
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            VideoWork wv = new VideoWork(panel1.Handle, 0, 0, panel1.Width, panel1.Height);
-            wv.Start();
+            //VideoWork wv = new VideoWork(panel1.Handle, 0, 0, panel1.Width, panel1.Height);
+            //wv.Start();
+
+            try
+            {
+                // 枚举所有视频输入设备
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                foreach (FilterInfo device in videoDevices)
+                {
+                    comboBox1.Items.Add(device.Name);
+                }
+
+                comboBox1.SelectedIndex = 0;
+
+            }
+            catch (ApplicationException)
+            {
+                comboBox1.Items.Add("No local capture devices");
+                videoDevices = null;
+            }
+
+            CameraConn();
+
+        }
+
+        private void CameraConn()
+        {
+            videoSource = new VideoCaptureDevice(videoDevices[comboBox1.SelectedIndex].MonikerString);
+            //videoSource.DesiredFrameSize = new Size(320, 240);
+            //videoSource.DesiredFrameRate = 1;
+
+            videoSourcePlayer1.VideoSource = videoSource;
+            videoSourcePlayer1.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            videoSourcePlayer1.SignalToStop();
+            videoSourcePlayer1.WaitForStop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (videoSource == null)
+                return;
+            Bitmap bitmap = videoSourcePlayer1.GetCurrentVideoFrame();
+            string fileName = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss-ff") + ".jpg";
+            bitmap.Save(Application.StartupPath + "/" + fileName, ImageFormat.Jpeg);
+            bitmap.Dispose();
         }
     }
 }
